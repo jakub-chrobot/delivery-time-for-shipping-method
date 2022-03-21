b28e1294aed19a4ebb29f4816195bf563db15328
jakub-chrobot <101723274+jakub-chrobot@users.noreply.github.com>
2022-03-21 10:16:17 +0100

    Version 0.2
    Choosing shipping method from select input

--- delivery-time-plugin.php
+++ delivery-time-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name:  Delivery time for shipping method - FLATE RATE
-Plugin URI:   
+Plugin URI:   https://github.com/jakub-chrobot/delivery-time-for-shipping-method
 Description:  Plugin to help assign time delivery to shipping method - WORKS FOR ONLY FLATE RATE
 Version:      0.1
 Author:       Jakub Chrobot
@@ -14,33 +14,53 @@ Text Domain:  czas-dostawy
 
 
 
-// display filter
+// hook
 add_action( 'woocommerce_after_shipping_rate', 'display', 10, 2 );
-
-// action hook
 add_action('wp_enqueue_scripts', 'style_load');
+add_action( 'carbon_fields_register_fields', 'delivery_plugin_options' );
+add_action( 'after_setup_theme', 'crb_load' );
+
 
 
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-add_action( 'carbon_fields_register_fields', 'delivery_plugin_options' );
+function crb_load() {
+    require_once( 'vendor/autoload.php' );
+    \Carbon_Fields\Carbon_Fields::boot();
+}
+
+
 function delivery_plugin_options() {
+
+    $delivery_id = array();
+    $delivery_name = array();
+    $i = 0; 
+
+    foreach ( delivermethods() as $dm ) {
+        $zone_shipping_methods = $dm->get_shipping_methods();
+        foreach ( $zone_shipping_methods as $index => $method ) {
+           $method_title = $method->get_title();
+           $method_rate_id = $method->get_rate_id(); 
+           $delivery_id[$i] = $method_rate_id;
+           $delivery_name[$i] = $method_title;
+           $i++;
+        }
+     }
+
+
+     $result = array_combine($delivery_id, $delivery_name);
+
     Container::make( 'theme_options', __( 'Delivery time for shipping method' ) )
         ->add_fields( array(
             Field::make( 'separator', 'separator_one', __( 'Custom description to delivery method' ) ),
             Field::make( 'complex', 'options', __( 'Assign description to delivery method' ) )
             ->add_fields( array(
                 Field::make( 'rich_text', 'comse', __( 'Description' ) ),
-                Field::make( 'text', 'id', __( 'Input ID for Flate Rate method Shipping' ) ),
-    ) ),
-            ));
-}
-
-add_action( 'after_setup_theme', 'crb_load' );
-function crb_load() {
-    require_once( 'vendor/autoload.php' );
-    \Carbon_Fields\Carbon_Fields::boot();
+                Field::make( 'select', 'crb_select', __( 'Choose shipping method' ) )
+                ->add_options( $result )
+                ) ),
+                ));
 }
 
 
@@ -59,9 +79,30 @@ function display ( $method, $index ) {
 
 }
 
-
 function style_load(){
 
     wp_enqueue_style( 'myStyles', plugins_url( 'simple-style.css', __FILE__ ) );
 
 }
+
+function delivermethods() {
+    $store = WC_Data_Store::load( 'shipping-zone' );
+    $raw = $store->get_zones();
+    foreach ( $raw as $zone ) {
+       $zones[] = new WC_Shipping_Zone( $zone );
+    }
+    $zones[] = new WC_Shipping_Zone( 0 );
+    return $zones;
+ 
+    foreach ( delivermethods() as $zone ) {
+        $zone_id = $zone->get_id();
+        $zone_name = $zone->get_zone_name();
+        $zone_order = $zone->get_zone_order();
+        $zone_locations = $zone->get_zone_locations();
+        $zone_formatted_location = $zone->get_formatted_location();
+        $zone_shipping_methods = $zone->get_shipping_methods();
+     }
+
+}
+ 
+ 
